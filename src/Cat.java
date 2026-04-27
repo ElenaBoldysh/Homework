ebcdd6856d703fea2bfed7631857c44e80aedb44
ElenaBoldysh <len5868@yandex.ru>
2026-04-27 16:35:06 +0400

    Update Cat.java

--- src/Cat.java
+++ src/Cat.java
@@ -2,10 +2,6 @@ public class Cat extends Animal {
     private int maxRunDistance = 200;
     private int maxSwimDistance = 0; //Коты не умеют плавать
 
-    /*public Cat(String name, int runDistance) {
-        super(name, runDistance);
-    }*/
-
     public void getInform(String name, int runDistance) {
         if (runDistance >= 0 && runDistance <= maxRunDistance) {
             System.out.println("Кот " + name + " пробежал " + runDistance + " м.");
