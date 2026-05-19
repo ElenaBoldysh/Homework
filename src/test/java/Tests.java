c8ece71e54db0e46781d09eca1de592eaaf003a3
ElenaBoldysh <len5868@yandex.ru>
2026-05-19 21:24:46 +0400

    Delete src/test/java/Tests.java

--- src/test/java/Tests.java
+++
@@ -1,9 +0,0 @@
-public class Tests {
-    protected WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = MtsOnlinePay.getDriver();
-        driver.get("https://www.mts.by/");
-    }
-}
